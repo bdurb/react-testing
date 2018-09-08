5695c27606646aec68be04e5bfd1501444a39521
bdurb <brian@rpmgreenville.com>
2018-09-08 15:18:43 -0700

    added connect into comment box component, and added action

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from 'actions';
 
 class CommentBox extends Component {
   state = {
@@ -11,7 +13,7 @@ class CommentBox extends Component {
 
   handleSubmit = e => {
     e.preventDefault()
-
+    this.props.saveComment(this.state.comment)
     this.setState({ comment: ''})
   }
   render() { 
@@ -27,4 +29,4 @@ class CommentBox extends Component {
   }
 }
  
-export default CommentBox;
+export default connect(null, actions)(CommentBox);
